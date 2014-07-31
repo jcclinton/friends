30406b1467aa51136bfe03fb88d533d05dce8b3d
Jamie Clinton <jcclinton24@gmail.com>
2014-07-31 12:03:51 -0700

    added indirect test with non-working test

--- tests/friends_tests.erl
+++ tests/friends_tests.erl
@@ -18,6 +18,11 @@ unmake_friends_test_() ->
 	 {setup, fun init_unmake1/0, fun stop/1, fun unmake_friends1/1}}
 	].
 
+%indirect_friends_test_() ->
+	%[{"indirect friends",
+	 %{setup, fun init_indirect1/0, fun stop/1, fun indirect_friends1/1}}
+	%].
+
 
 
 
@@ -50,6 +55,17 @@ init_unmake1() ->
 	{[Aaron, Bella, David, Cindy], "David"}.
 
 
+init_indirect1() ->
+	friends:init(),
+	% friends to check
+	Aaron = {"Aaron", ["Bella"]},
+	Bella = {"Bella", ["Cindy"]},
+	Cindy = {"Cindy", ["David"]},
+
+	% list of people and who they should make friends with
+	{[Aaron, Bella, Cindy], "Aaron"}.
+
+
 stop(_SetupData) ->
 	friends:cleanup().
 
@@ -67,6 +83,7 @@ make_friends1({PeopleList, Person}) ->
 	% sort them just to ensure lists will be in the same order
 	[?_assertEqual( lists:sort(DirectFriendList), lists:sort(["Bella", "Elizabeth"]))].
 
+
 unmake_friends1({PeopleList, Person}) ->
 	make_all_friends(PeopleList),
 
@@ -75,6 +92,15 @@ unmake_friends1({PeopleList, Person}) ->
 	% test that these two friends are on this list
 	% sort them just to ensure lists will be in the same order
 	[?_assertEqual( lists:sort(DirectFriendList), lists:sort(["Elizabeth"]))].
+
+
+indirect_friends1({PeopleList, Person}) ->
+	make_all_friends(PeopleList),
+
+	IndirectFriendsList = friends:get_indirect(Person, Person),
+	% test that these two friends are on this list
+	% sort them just to ensure lists will be in the same order
+	[?_assertEqual( lists:sort(IndirectFriendsList), lists:sort(["Cindy", "David"]))].
 
 
 
